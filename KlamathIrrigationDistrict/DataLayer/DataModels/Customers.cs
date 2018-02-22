18b5488709099a32c2158d44abb1651f8158471a
Nathan Banavong <nathan.banavong@oit.edu>
2018-02-22 07:45:44 -0800

    Nathan Commit on Customers, issues with not seeing the Current requests for customers

--- KlamathIrrigationDistrict/DataLayer/DataModels/Customers.cs
+++ KlamathIrrigationDistrict/DataLayer/DataModels/Customers.cs
@@ -9,6 +9,7 @@ namespace KlamathIrrigationDistrict.DataLayer.DataModels
     //Customers Table
     public class Customers
     {
+        //-------------------------------------------------------------------------------------
         //Customers(int CustomerID, int TrackingID, String Name, Decimal TotalAllotment)
         [Required]
         public int CustomerID { get; set; }
@@ -16,14 +17,6 @@ namespace KlamathIrrigationDistrict.DataLayer.DataModels
         public int TrackingID { get; set; }
         [Required]
         public string Name { get; set; }
-
-        //3 var constructor created to help pass the info for customers into the requests
-        //public CustomersInfo (int ID, int TrackingNum, string CustomerName)
-        //{
-        //    ID = CustomerID;
-        //    TrackingNum = TrackingID;
-        //    CustomerName = Name;
-        //}
 
         [Required]
         public string Address1 { get; set; }
@@ -36,42 +29,84 @@ namespace KlamathIrrigationDistrict.DataLayer.DataModels
         public int Zip { get; set; }
         public decimal TotalAllotment { get; set; }
         public List<Customers> customers { get; set; }
-        
-        //public Customers ( int PersonID, string CustomerName, int TrackNumber)
-        //{
-        //    CustomerID = PersonID;
-        //    Name = CustomerName;
-        //    TrackingID = TrackNumber;
-        //}
-        
-    }
+        //-------------------------------------------------------------------------------------
 
-    //WaterOrderRequest Class would inherit the customerID from Customers
-    public class WaterOrderRequest : Customers
-    {
-        public decimal RequestedCFS { get; set; }
-
-        public string CustomerComments { get; set; }
-
-        //this is the ID for the water request itself
+        //List of items to allow input for requests
         public int RequestID { get; set; }
 
         public string Structure { get; set; }
 
-        //need to find datatype acceptable for date -> datetime 
-        //public (date) CustomerDate {get; set;}
+        //references 'CustomerCFS1 - meaning on
+        public decimal CustomerCFS_1 { get; set; }
 
-        //constructor to pass Variables into WaterOrder
-        public WaterOrderRequest
-            (int personID, string CustomerName, int tracknumber, decimal CFS, string Comments, decimal Allotment, string structureID)
-        {
-            personID = CustomerID;
-            CustomerName = Name;
-            tracknumber = TrackingID;
-            CFS = RequestedCFS;
-            Comments = CustomerComments;
-            Allotment = TotalAllotment;
-            structureID = Structure;
-        }
+        //rerence the first pair of comments when water turned on
+        public string CustomerComments_1 { get; set; }
+
+        //date submit of requests
+        public DateTime TimeStampCustomer1 { get; set; }
+
+        //date in which customer would like the water turned off
+        public DateTime CustomerDate1 { get; set; }
+
+        public DateTime _GetCustomerDate = DateTime.Now;
+
+        //will display 'confirm' if completed task
+        public string RequestStatus1 { get; set; }
+
+        //reference 'customercfs2' - water turned on again
+        public int CustomerCFS_2 { get; set; }
+
+        //second set of comments when customer wants to use more water
+        public string CustomerComments_2 { get; set; }
+
+        //date of submit request 2 for same plot of land
+        public DateTime TimeStampCustomer2 { get; set; }
+
+        //end of timestamp 2 for request
+        public DateTime CustomerDate2 { get; set; }
+
+        //the request status of second round of applied request
+        public String RequestStatus2 { get; set; }
+
+
+        //-------------------------------------------------------------------------------------
+        //DitchRider Variables for Customer to see when viewing the customer history
+        
+        //The staff id for completing task - first set of water usage
+        public int KIDStaffID_1 { get; set; }
+
+        //name that links with staffID
+        public String StaffName_1 { get; set; }
+
+        //time at which rider accepted or rejected water request
+        public DateTime TimeStampStaff1 { get; set; }
+
+        //time staf member completed the task if accepted it
+        public DateTime StaffDate1 { get; set; }
+
+        //will be referenced when customer viewing history of applied CFS
+        public int StaffCFS1 { get; set; }
+
+        //will be referenced to allow customer to view staff comments
+        public string StaffComments1 { get; set; }
+
+        //the second id referenceing staff on a request
+        public int KIDStaffID_2 { get; set; }
+
+        //reference ID, display actual staff name
+        public string StaffName_2 { get; set; }
+
+        //second time of rider accepting or rejecting request
+        public DateTime TimeStampStaff2 { get; set; }
+
+        //date in which ditchrider completed task
+        public DateTime StaffDate2 { get; set; }
+
+        //will be referenced when customer viewing history of applied CFS
+        public int StaffCFS2 { get; set; }
+
+        //will be referenced to allow customer to view staff comments
+        public string StaffComments2 { get; set; }
+
     }
 }
--- KlamathIrrigationDistrict/DataLayer/Repositories/CustomerRepository.cs
+++ KlamathIrrigationDistrict/DataLayer/Repositories/CustomerRepository.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System;
 
 namespace KlamathIrrigationDistrict.DataLayer.Repositories
 {
@@ -43,30 +44,30 @@ namespace KlamathIrrigationDistrict.DataLayer.Repositories
             return (s);
         }
 
-        public virtual Customers GetTrackingID(int TrackingID)
-        {
-            Customers C_TrackID = null;
-            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings[@"KlamathIrrigation_Test"].ConnectionString))
-            {
-                using (SqlCommand command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandType = CommandType.Text;
-                    command.CommandText = "SELECT TrackingID FROM Customers";
-                    command.Parameters.AddWithValue("@TrackingID", TrackingID);
-                    connection.Open();
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            C_TrackID = new Customers();
-                            C_TrackID.TrackingID = int.Parse(reader["TrackingID"].ToString());                            
-                        }
-                    }
-                }
-            }
-            return (C_TrackID);
-        }
+        //public virtual Customers GetTrackingID(int TrackingID)
+        //{
+        //    Customers C_TrackID = null;
+        //    using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings[@"KlamathIrrigation_Test"].ConnectionString))
+        //    {
+        //        using (SqlCommand command = new SqlCommand())
+        //        {
+        //            command.Connection = connection;
+        //            command.CommandType = CommandType.Text;
+        //            command.CommandText = "SELECT TrackingID FROM Customers";
+        //            command.Parameters.AddWithValue("@TrackingID", TrackingID);
+        //            connection.Open();
+        //            using (SqlDataReader reader = command.ExecuteReader())
+        //            {
+        //                if (reader.Read())
+        //                {
+        //                    C_TrackID = new Customers();
+        //                    C_TrackID.TrackingID = int.Parse(reader["TrackingID"].ToString());                            
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return (C_TrackID);
+        //}
 
         public virtual List<Customers> ViewCustomers()
         {
@@ -99,6 +100,56 @@ namespace KlamathIrrigationDistrict.DataLayer.Repositories
                 }
             }
             return (vc);
+        }
+
+        public List<Customers> ViewAppliedRequests()
+        {
+            List<Customers> RequestList = new List<Customers>();
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["KlamathIrrigation_Test"].ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    //command.CommandText = "SELECT * FROM Requests";
+                    command.CommandText = "SELECT * FROM Requests";
+                    command.CommandType = CommandType.Text;
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Customers p = new Customers();
+                            p.RequestID = int.Parse(reader["RequestID"].ToString());
+                            p.TimeStampCustomer1 = DateTime.Parse(reader["TimeStampCustomer1"].ToString());
+                            p.CustomerDate1 = DateTime.Parse(reader["CustomerDate1"].ToString());
+                            p.CustomerID = int.Parse(reader["CustomerID"].ToString());
+                            p.Name = reader["CustomerName"].ToString();
+                            p.Structure = reader["Structure"].ToString();
+                            p.CustomerCFS_1 = int.Parse(reader["CustomerCFS1"].ToString());
+                            p.CustomerComments_1 = reader["CustomerComments1"].ToString();
+                            p.TimeStampStaff1 = DateTime.Parse(reader["TimeStampStaff1"].ToString());
+                            p.KIDStaffID_1 = int.Parse(reader["KIDStaffID1"].ToString());
+                            p.StaffName_1 = reader["StaffName1"].ToString();
+                            p.StaffDate1 = DateTime.Parse(reader["StaffDate1"].ToString());
+                            p.RequestStatus1 = reader["RequestStatus1"].ToString();
+                            p.StaffCFS1 = int.Parse(reader["StaffCFS1"].ToString());
+                            p.StaffComments1 = reader["StaffComments1"].ToString();
+                            p.TimeStampCustomer2 = DateTime.Parse(reader["TimeStampCustomer2"].ToString());
+                            p.CustomerDate2 = DateTime.Parse(reader["CustomerDate2"].ToString());
+                            p.CustomerCFS_2 = int.Parse(reader["CustomerCFS2"].ToString());
+                            p.CustomerComments_2 = reader["CustomerCommetns2"].ToString();
+                            p.TimeStampStaff2 = DateTime.Parse(reader["TimeStampStaff2"].ToString());
+                            p.KIDStaffID_2 = int.Parse(reader["KIDStaffID2"].ToString());
+                            p.StaffName_2 = reader["StaffName2"].ToString();
+                            p.StaffDate2 = DateTime.Parse(reader["StaffDate2"].ToString());
+                            p.RequestStatus2 = reader["RequestStatus2"].ToString();
+                            p.StaffCFS2 = int.Parse(reader["StaffCFS2"].ToString());
+                            p.StaffComments2 = reader["StaffComments2"].ToString();
+                        }
+                    }
+                }
+            }
+            return (RequestList);
         }
 
         //use the stored procedure to display the customer's water history
@@ -179,7 +230,7 @@ namespace KlamathIrrigationDistrict.DataLayer.Repositories
         //}
         //-------------------------------------------------------------------------------------------------------------------
 
-        public virtual void AddWaterOrderRequest(WaterOrderRequest NewWaterOrder)
+        public virtual void AddWaterOrderRequest(Customers NewWaterOrder)
         {
             //find a better way to Apply request for Water Order
             //WaterOrderRequest NewOrder = null;
@@ -191,21 +242,47 @@ namespace KlamathIrrigationDistrict.DataLayer.Repositories
                     command.CommandType = CommandType.Text;
                     command.CommandText = 
                         "INSERT INTO Requests" + 
-                        "(CustomerID, TrackingID, CustomerName, Structure, CustomerDate, CFSRequested, CustomerComments" +
-                        "VALUES (@CustomerID, @TrackingID, @CustomerName, @Structure, @CustomerDate, @CFSRequested, @CustomerComments";
+                        "(CustomerID, CustomerName, Structure, CustomerCFS1, TimeStampCustomer1, " +
+                        "CustomerDate1, CustomerComments1" +
+                        "VALUES (@CustomerID, @CustomerName, @Structure, @TimeStampCustomer1, @CustomerDate1, @CustomerCFS1, @CustomerComments1";
                     command.Parameters.AddWithValue("@CustomerID", NewWaterOrder.CustomerID);
-                    command.Parameters.AddWithValue("@TrackingID", NewWaterOrder.TrackingID);
                     command.Parameters.AddWithValue("@CustomerName", NewWaterOrder.Name);
                     command.Parameters.AddWithValue("@Structure", NewWaterOrder.Structure);
                     //use getdate parameter to fulfill the customerdate
-                    command.Parameters.AddWithValue("@CustomerDate", "GETDATE()");
-                    command.Parameters.AddWithValue("@CFSRequested", NewWaterOrder.RequestedCFS);
-                    command.Parameters.AddWithValue("@CustomerComments", NewWaterOrder.CustomerComments);
+                    command.Parameters.AddWithValue("@TimeStampCustomer1", NewWaterOrder.TimeStampCustomer1);
+                    command.Parameters.AddWithValue("@CustomerDate1", NewWaterOrder._GetCustomerDate);
+                    command.Parameters.AddWithValue("@CustomerCFS1", NewWaterOrder.CustomerCFS_1);
+                    command.Parameters.AddWithValue("@CustomerComments1", NewWaterOrder.CustomerComments_1);
                     connection.Open();
                     command.ExecuteNonQuery();
                 }
             }
         }
+
+        //public virtual void AddRequest(Customers WaterOrder)
+        //{
+        //    using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["KlamathIrrigation_Test"].ConnectionString))
+        //    {
+        //        using (SqlCommand command = new SqlCommand())
+        //        {
+        //            command.Connection = connection;
+        //            command.CommandType = CommandType.StoredProcedure;
+        //            command.CommandText = "sp_DitchRider_AddRequests";
+
+        //            command.Parameters.AddWithValue("@CustomerID", WaterOrder.CustomerID);
+        //            command.Parameters.AddWithValue("@TrackingID", WaterOrder.TrackingID);
+        //            command.Parameters.AddWithValue("@CustomerName", WaterOrder.CustomerName);
+        //            command.Parameters.AddWithValue("@Allotment", WaterOrder.Allotment);
+        //            command.Parameters.AddWithValue("@MapTaxLot", WaterOrder.MapTaxLot);
+        //            command.Parameters.AddWithValue("@Structure", WaterOrder.Structure);
+        //            command.Parameters.AddWithValue("@Customerdate", WaterOrder.CustomerDate);
+        //            command.Parameters.AddWithValue("@CFSRequeted", WaterOrder.CFSRequested);
+        //            command.Parameters.AddWithValue("@CustomerComments", WaterOrder.CustomerComments);
+        //            connection.Open();
+        //            command.ExecuteNonQuery();
+        //        }
+        //    }
+        //}
 
 
         //public static void AddProduct(Product product)
